e46cfaef448ca399626a1c2020ee2ad5dc788645
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-14 18:00:07 +0200

    222

--- npmScripts/src/scripts/logger/logger.js
+++ npmScripts/src/scripts/logger/logger.js
@@ -12,3 +12,13 @@ export const createLogger = (name) => {
     },
   };
 };
+
+// "scripts": {
+//     "test": "jest",
+//     "test:watch": "jest --watch",
+//     "build:styles": "sass ./src/styles/index.scss ./dist/styles.css",
+//     "copy:html": "cp ./src/index.html ./dist/index.html",
+//     "copy:scripts": "cp -R ./src/scripts/ ./dist/scripts",
+//     "clean": "rm -rf ./dist",
+//     "build": "npm-run-all clean build:styles copy:*"
+//   },
