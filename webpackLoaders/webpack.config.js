4eee82ff0da47fb644b255a7adfa0e62031b2eb6
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-17 14:52:48 +0200

    33333

---
+++ webpackLoaders/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode,
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "assets/images",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  //   devtool: "eval-source-maps",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+      },
+      //   {
+      //     test: /\.js$/,
+      //     enforce: "pre",
+      //     use: ["source-map-loader"],
+      //   },
+    ],
+  },
+};
