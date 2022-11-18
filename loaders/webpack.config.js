e7c42f5d004f8d1fa01165f242d31079992c6a43
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-18 14:22:31 +0200

    44343

--- loaders/webpack.config.js
+++ loaders/webpack.config.js
@@ -1,38 +1,56 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("0.8.0");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /.s?css$/,
-        use: ["MiniCssExtractPlugin.loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /.(jpg|png)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-              name: "[name].[ext]",
-              outputPath: "images",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? "MiniCssExtractPlugin.loader" : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /.(jpg|png)$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 8192,
+                name: "[name].[ext]",
+                outputPath: "images",
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve("./src/index.html"),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve("./src/index.html"),
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-  ],
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
 };
